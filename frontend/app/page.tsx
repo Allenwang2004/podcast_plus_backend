80a3bd8d2bb570e3fa53c044dce5d74b24879173
Allenwang2004 <ziyiw1930@gmail.com>
2026-02-26 15:57:05 +0800

    feat: modify rag pipline and processing tracker for knowledge update

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PodcastInterface } from "@/components/podcast-interface"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <PodcastInterface />
-    </main>
-  )
-}
